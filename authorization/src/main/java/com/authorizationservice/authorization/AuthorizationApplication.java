61782b1e1f4053668a7a26e630674751997f7db5
muska <muska@DESKTOP-39E9FIS>
2022-07-07 15:31:07 +0530

    First commit.

---
+++ authorization/src/main/java/com/authorizationservice/authorization/AuthorizationApplication.java
@@ -0,0 +1,41 @@
+package com.authorizationservice.authorization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.authorizationservice.authorization.model.AuthenticationRequest;
+import com.authorizationservice.authorization.repository.AuthRequestRepo;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class AuthorizationApplication implements CommandLineRunner{
+	
+	@Autowired
+	AuthRequestRepo authRequestRepo;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		AuthenticationRequest user1 = new AuthenticationRequest("muskan","muskan01");
+		AuthenticationRequest user2 = new AuthenticationRequest("haritha","haritha01");
+		AuthenticationRequest user3 = new AuthenticationRequest("harshit","harshit01");
+		AuthenticationRequest user4 = new AuthenticationRequest("sravya","sravya01");
+		AuthenticationRequest user5 = new AuthenticationRequest("diksha","diksha01");
+		
+		
+		
+		authRequestRepo.save(user1);
+		authRequestRepo.save(user2);
+		authRequestRepo.save(user3);
+		authRequestRepo.save(user4);
+		authRequestRepo.save(user5);
+	}
+
+}
